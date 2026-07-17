e00484688b16e936308ed5d4045370287fe6c36b
crlisbelvinn <carlisbelvinwaruwu@gmail.com>
2026-07-17 14:52:32 +0700

    feat(category): complete category CRUD

--- backend-petalerp/app/Models/Category.php
+++ backend-petalerp/app/Models/Category.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Category extends Model
 {
@@ -10,5 +11,10 @@ class Category extends Model
         'name',
         'description',
         'is_active',
-    ];  
+    ];
+
+    public function products(): HasMany
+    {
+        return $this->hasMany(Product::class);
+    }
 }
--- backend-petalerp/app/Models/Product.php
+++ backend-petalerp/app/Models/Product.php
@@ -3,8 +3,21 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Product extends Model
 {
-    //
+    protected $fillable = [
+        'category_id',
+        'product_name',
+        'price',
+        'stock',
+        'description',
+        'picture',
+    ];
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
